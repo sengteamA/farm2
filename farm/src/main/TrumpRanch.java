fdc4dfb3f367488721ea0dd409a6a510d943e6e2
AbysmalMonkey <nicklee9999@hotmail.com>
2020-04-20 19:58:43 +1200

    Tidied up overlapping import & methods

--- farm/src/main/TrumpRanch.java
+++ farm/src/main/TrumpRanch.java
@@ -1,47 +1,16 @@
 package main;
 
-import java.util.ArrayList;
-
 public class TrumpRanch extends Farm {
 	public float discount = (float)0.9; //10% discount on all purchases
 	public float bonus = (float)1.1; //10% bonus on selling crops
-	public int bankBalance = 1200; // 20% bonus starting cash
+	public int bankBalance = 1200; //hard coded 20% starting bonus
+	
 	TrumpRanch() {
 		super();
 	}
 	
-	public String getName() {
-		return super.getName();
-	}
-	
 	public int getBankBalance() {
 		return bankBalance;
-	}
-	
-	public int getActionsLeft() {
-		return super.getActionsLeft();
-	}
-	
-	/*
-	 * animal, crops, items not yet implemented
-	public ArrayList showCrops() {
-		return super.showCrops();
-	}
-	
-	public Arraylist showAnimals() {
-		return super.showAnimals();
-	}
-	
-	public Arraylist showItems() {
-		return super.showItems();
-	}*/
-	
-	public void setName() {
-		super.setName();
-	}
-	
-	public void updateAP() {
-		super.updateAP();
 	}
 	
 	public void updateBankbalance(int amount) {
@@ -49,7 +18,7 @@ public class TrumpRanch extends Farm {
 	}
 	
 	public String Flavour() {
-		return "I always get the best deals!!\n"
+		return "NOBODY KNOWS MORE ABOUT GETTING GOOD DEALS THAN ME!!\n"
 				+ "All purchases are 10% cheaper\n"
 				+ "10% bonus on all sales\n"
 				+ "Starts game with 20% more cash";
