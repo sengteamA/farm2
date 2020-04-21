fe080930fd8af977f858b08a497cb55b7a91741c
Grant Wong <git@wong.nz>
2020-04-21 15:28:31 +1200

    Fix some more references in Animal tests

--- farm/test/main/animal/AnimalTest.java
+++ farm/test/main/animal/AnimalTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.Farm;
+
 class AnimalTest {
 	
 	public Animal pig;
--- farm/test/main/animal/SheepTest.java
+++ farm/test/main/animal/SheepTest.java
@@ -2,6 +2,8 @@ package main.animal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import main.Farm;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
