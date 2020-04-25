b9e97fae35fde30cd90bb0464878543a8b936e8e
Grant Wong <git@wong.nz>
2020-04-25 19:34:50 +1200

    Add very very basic Farmer class

---
+++ farm/src/main/Farmer.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Farmer {
+	private String name;
+	// TODO: I don't remember what this was for, possibly remove later
+	// private String type;
+	private int age; // TODO check if necessary
+
+	Farmer() {
+		
+	}
+}
