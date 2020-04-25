777c6b13e13bdb8b8f281992e271a02851310695
AbysmalMonkey <nicklee9999@hotmail.com>
2020-04-25 21:05:53 +1200

    corrected String attribute - oops

--- farm/src/main/TomaccoLand.java
+++ farm/src/main/TomaccoLand.java
@@ -2,7 +2,7 @@ package main;
 
 public class TomaccoLand extends Farm {
 	public float cropGrowth = (float)1.2;
-	public Strin "The dangers of nuclear waste meets the genius of genetic engineering\n"
+	public String flavour = "The dangers of nuclear waste meets the genius of genetic engineering\n"
 			+ "tending to crops is 20% more effective\n"
 			+ "droughts are 20% less likely";
 	
