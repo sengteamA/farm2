08a8051e14730c1a85a1107e0a1ecf9bd8045954
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-08 17:05:02 +1200

    updated daily actions for Farmer
    + relevant farmer tests
    still to come...
    tend to crops

--- farm/src/main/Farmer.java
+++ farm/src/main/Farmer.java
@@ -56,4 +56,66 @@ public class Farmer {
 		}
 		System.out.printf("Got it. You are %d years old.\n", age);
 	}
+	
+	public void feedAnimals(Item item) {
+		float health = 0;
+		if (item.getName().equals("Stockfeed")) {
+			health = 50;
+			}
+		else if (item.getName().contentEquals("Panda Gummy")) {
+			health = 100;
+		}
+		if (farm instanceof AnimalFarm) {
+			health += health * ((AnimalFarm) farm).animal_bonus; 
+		}
+		for (Animal animal: farm.showAnimals()) {
+			animal.updateHealth((int)health);
+		}
+		farm.delItem(item);
+		farm.updateAP();
+	}
+	
+	public void playWithAnimals() {
+		for (Animal animal: farm.showAnimals()) {
+			float happy = 30;
+			if (farm instanceof AnimalFarm) {
+				happy += happy * ((AnimalFarm) farm).animal_bonus;
+			}
+			animal.updateHappiness((int)happy);
+		}
+		farm.updateAP();
+	}
+	
+	public void harvestCrops() {
+		float revenue = 0;
+		for (Crop crop: farm.showCrops()) {
+			if (crop.getDaysLeft() <= 0) {
+				revenue += crop.getPrice();
+			}
+		}
+		if (farm instanceof TrumpRanch) {
+			revenue = revenue * ((TrumpRanch) farm).bonus;
+			((TrumpRanch) farm).updateBankbalance((int)revenue);
+		}
+		farm.updateBankBalance((int)revenue);
+		farm.showCrops().removeIf(crop -> crop.getDaysLeft() == 0);
+		farm.updateAP();
+	}
+	
+	public void tendToLand() {
+		for (Animal animal: farm.showAnimals()) {
+			float happy = 20;
+			if (farm instanceof AnimalFarm) {
+				happy += happy * ((AnimalFarm) farm).animal_bonus;
+			}
+			animal.updateHappiness((int)happy);
+		}
+		farm.addCap(2);
+		farm.updateAP();
+	}
+	
+	public static void main(String[] args) {
+		PandaGummy panda = new PandaGummy();
+		Cow moomoo = new Cow();
+	}
 }
