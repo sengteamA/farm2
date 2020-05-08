3ba1ca969057990e475a59d9eb11ad0dc516e827
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-08 20:28:25 +1200

    Changes to Tomacco Farm
    Tomacco farm test deleted as no longer required
    20% bonus replaced with +1 day.

--- farm/src/main/farms/TomaccoLand.java
+++ farm/src/main/farms/TomaccoLand.java
@@ -1,11 +1,9 @@
 package main.farms;
 
 public class TomaccoLand extends Farm {
-	public float cropGrowth = (float)1.2;
-
 	public TomaccoLand() {
 		super("Tomacco Land",
 				"The dangers of nuclear waste meets the genius of genetic engineering\n"
-				+ "tending to crops is 20% more effective");
+				+ "tending to crops makes them mature 1 day faster than others");
 	}
 }
