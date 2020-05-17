01c6f8f3d072a05aaf772f2cf3d11aeac5be2dd4
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-17 21:33:47 +1200

    Partially reworked farmGUI. Game ends correctly with pop-up window.

--- farm/src/main/FarmGUI.java
+++ farm/src/main/FarmGUI.java
@@ -1,120 +1,210 @@
 package main;
 
 import java.awt.EventQueue;
+
+import main.animals.*;
+import main.crops.*;
 import main.farms.*;
+import main.items.*;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextPane;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
 
 public class FarmGUI {
 
-	private JFrame frame;
+	private JFrame frmFarmOverview;
+	private GameManager manager;
 	private JTextField txtDays;
 	private JTextField textField;
 	private JTextField txtMoney;
 	private JTextField txtFarmerName;
 	private JTextField txtCapacity;
-	private Farm farm;
+	private JTextField landCapacity;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FarmGUI window = new FarmGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the application.
 	 */
-	public FarmGUI() {
+	public FarmGUI(GameManager myManager) {
+		manager = myManager;
 		initialize();
+		frmFarmOverview.setVisible(true);
 	}
+	
+	/**
+	 * Closes this window (GameSetupWindow instance).
+	 */
+	public void closeWindow() {
+		frmFarmOverview.dispose();
+	}
+	
+	/**
+	 * Calls the parent game manager, which closes this window and creates
+	 * the next one that's needed.
+	 */
+	public void finishedWindow() {
+		manager.closeFarmScreen(this);
+	}
+	
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setLayout(null);
+		frmFarmOverview = new JFrame();
+		frmFarmOverview.setTitle("Farm Overview");
+		frmFarmOverview.getContentPane().setLayout(null);
+		frmFarmOverview.setBounds(100, 100, 600, 450);
 		
-		txtDays = new JTextField();
-		txtDays.setHorizontalAlignment(SwingConstants.CENTER);
-		txtDays.setText("Farm name");
-		txtDays.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		txtDays.setBounds(58, 27, 159, 52);
-		frame.getContentPane().add(txtDays);
-		txtDays.setColumns(10);
+		JLabel lblLandCap = new JLabel("Land Capacity: " + manager.farm.getFarmCap());
+		lblLandCap.setHorizontalAlignment(SwingConstants.LEFT);
+		lblLandCap.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblLandCap.setBounds(417, 84, 137, 34);
+		frmFarmOverview.getContentPane().add(lblLandCap);
 		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setText("Days");
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		textField.setColumns(10);
-		textField.setBounds(58, 89, 159, 52);
-		frame.getContentPane().add(textField);
+		JLabel lblDay = new JLabel("Day: " + manager.dayNumber + "/" + manager.maxDays);
+		lblDay.setHorizontalAlignment(SwingConstants.LEFT);
+		lblDay.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblDay.setBounds(417, 119, 137, 34);
+		frmFarmOverview.getContentPane().add(lblDay);
 		
-		JButton btnNewButton = new JButton("View animals");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnNewButton.setBounds(56, 312, 160, 61);
-		frame.getContentPane().add(btnNewButton);
+		JLabel lblFarmMoney = new JLabel("$ " + manager.farm.getBankBalance());
+		lblFarmMoney.setHorizontalAlignment(SwingConstants.LEFT);
+		lblFarmMoney.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblFarmMoney.setBounds(417, 149, 137, 34);
+		frmFarmOverview.getContentPane().add(lblFarmMoney);
 		
-		JButton btnDailyAction = new JButton("Daily Action");
-		btnDailyAction.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnDailyAction.setBounds(57, 230, 160, 61);
-		frame.getContentPane().add(btnDailyAction);
+		JLabel lblActions = new JLabel("Actions Left: " + manager.farm.getActionsLeft());
+		lblActions.setHorizontalAlignment(SwingConstants.LEFT);
+		lblActions.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblActions.setBounds(417, 179, 137, 42);
+		frmFarmOverview.getContentPane().add(lblActions);
 		
-		JButton btnViewCrops = new JButton("View crops");
-		btnViewCrops.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnViewCrops.addActionListener(new ActionListener() {
+		JButton btnCrops = new JButton("Tend to Crops");
+		btnCrops.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnCrops.setBounds(31, 28, 155, 48);
+		frmFarmOverview.getContentPane().add(btnCrops);
+		
+		JButton btnPlayWithAnimals = new JButton("Play with Animals");
+		btnPlayWithAnimals.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnPlayWithAnimals.setBounds(31, 84, 155, 48);
+		frmFarmOverview.getContentPane().add(btnPlayWithAnimals);
+		
+		JButton btnFeedAnimals = new JButton("Feed Animals");
+		btnFeedAnimals.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnFeedAnimals.setBounds(31, 142, 155, 48);
+		frmFarmOverview.getContentPane().add(btnFeedAnimals);
+		
+		JButton button = new JButton("Tend to Crops");
+		button.setFont(new Font("Tahoma", Font.BOLD, 12));
+		button.setBounds(196, 28, 155, 48);
+		frmFarmOverview.getContentPane().add(button);
+		
+		JButton btnHarvestCrops = new JButton("Harvest Crops");
+		btnHarvestCrops.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnHarvestCrops.setBounds(196, 84, 155, 48);
+		frmFarmOverview.getContentPane().add(btnHarvestCrops);
+		
+		JButton btnVisitStore = new JButton("Visit Store");
+		btnVisitStore.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnVisitStore.setBounds(196, 142, 155, 48);
+		frmFarmOverview.getContentPane().add(btnVisitStore);
+
+		String assets[] = {"", "Animals", "Crops", "Items"};
+		JComboBox comboBox = new JComboBox(assets);
+		comboBox.setBounds(31, 267, 111, 34);
+		frmFarmOverview.getContentPane().add(comboBox);
+		
+		JList listsDisplay = new JList();
+		listsDisplay.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		listsDisplay.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		listsDisplay.setBounds(152, 226, 199, 157);
+		frmFarmOverview.getContentPane().add(listsDisplay);
+		
+		JButton btnViewAssets = new JButton("View Assets");
+		btnViewAssets.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				if (comboBox.getSelectedItem().equals("Animals")) {
+					DefaultListModel<Animal> animalListModel = new DefaultListModel<>();
+					animalListModel.addAll(manager.farm.showAnimals());
+					listsDisplay.setModel(animalListModel);
+				}
+				else if (comboBox.getSelectedItem().equals("Crops")) {
+					DefaultListModel<Crop> cropListModel = new DefaultListModel<>();
+					cropListModel.addAll(manager.farm.showCrops());
+					listsDisplay.setModel(cropListModel);
+				}
+				else if (comboBox.getSelectedItem().equals("Items")) {
+					DefaultListModel<Item> itemListModel = new DefaultListModel<>();
+					itemListModel.addAll(manager.farm.showItems());
+					listsDisplay.setModel(itemListModel);
+				}
+				else {
+					DefaultListModel<String> placeHolder = new DefaultListModel<String>();
+					listsDisplay.setModel(placeHolder);
+				}
 			}
 		});
-		btnViewCrops.setBounds(56, 393, 160, 61);
-		frame.getContentPane().add(btnViewCrops);
 		
-		JButton btnViewItems = new JButton("View items");
-		btnViewItems.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnViewItems.setBounds(57, 475, 160, 61);
-		frame.getContentPane().add(btnViewItems);
+		btnViewAssets.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnViewAssets.setBounds(31, 226, 111, 42);
+		frmFarmOverview.getContentPane().add(btnViewAssets);
 		
-		txtMoney = new JTextField();
-		txtMoney.setText("$ Money");
-		txtMoney.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		txtMoney.setHorizontalAlignment(SwingConstants.CENTER);
-		txtMoney.setBounds(542, 43, 169, 64);
-		frame.getContentPane().add(txtMoney);
-		txtMoney.setColumns(10);
 		
-		txtFarmerName = new JTextField();
-		txtFarmerName.setText("Farmer name");
-		txtFarmerName.setHorizontalAlignment(SwingConstants.CENTER);
-		txtFarmerName.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		txtFarmerName.setColumns(10);
-		txtFarmerName.setBounds(552, 134, 159, 52);
-		frame.getContentPane().add(txtFarmerName);
+		JLabel FarmStatus = new JLabel("Farm Status");
+		FarmStatus.setHorizontalAlignment(SwingConstants.LEFT);
+		FarmStatus.setFont(new Font("Tahoma", Font.BOLD, 18));
+		FarmStatus.setBounds(417, 40, 137, 34);
+		frmFarmOverview.getContentPane().add(FarmStatus);
 		
-		txtCapacity = new JTextField();
-		txtCapacity.setText("Capacity");
-		txtCapacity.setHorizontalAlignment(SwingConstants.CENTER);
-		txtCapacity.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		txtCapacity.setColumns(10);
-		txtCapacity.setBounds(58, 151, 159, 52);
-		frame.getContentPane().add(txtCapacity);
+		JButton btnDayEnd = new JButton("End Day");
+		btnDayEnd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				++manager.dayNumber;
+				if (manager.dayNumber > (manager.maxDays)) {
+					finishedWindow();
+					JOptionPane.showMessageDialog(frmFarmOverview, "==========\nYou have completed the farm simulator!\nThank you for playing.\n==========\n"
+							+ "Farm name: " + manager.farm.getName() + "\n" + "Game duration: " + (manager.dayNumber-1) + "\n" + "Profit: $" + manager.farm.getBankBalance());
+				}
+				manager.farm.refreshAP();
+				int income = manager.farm.getDailyBonusMoney();
+				manager.farm.updateBankBalance(income);
+				for (Crop crop: manager.farm.showCrops()) {
+					crop.updateDaysElapsed(1);
+				}
+				lblDay.setText("Day: " + manager.dayNumber + "/" + manager.maxDays);
+				lblFarmMoney.setText("$ " + manager.farm.getBankBalance());
+				lblActions.setText("Actions Left: " + manager.farm.getActionsLeft());
+				comboBox.setSelectedIndex(0);
+				DefaultListModel<String> placeHolder = new DefaultListModel<String>();
+				listsDisplay.setModel(placeHolder);
+			}
+		});
+		btnDayEnd.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnDayEnd.setBounds(399, 335, 155, 48);
+		frmFarmOverview.getContentPane().add(btnDayEnd);
+		
+		
+		
 	}
 }
