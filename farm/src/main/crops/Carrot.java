eaa9dbba616e33acbb7fb481797060f775508391
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-19 16:06:15 +1200

    Added JavaDoc for carrot

--- farm/src/main/crops/Carrot.java
+++ farm/src/main/crops/Carrot.java
@@ -1,3 +1,12 @@
+/**
+ * This is an extension of the Crop class.
+ * It is an item that can be bought in game.
+ * Once its daystoHarvest reaches 0, it can be harvested for money.  
+ * 
+ * @author Grant Wong
+ */
+
+
 package main.crops;
 
 public class Carrot extends Crop {
