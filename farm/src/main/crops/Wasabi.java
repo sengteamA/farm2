cbacc82be0f8c27ff3f41f968cc28cd97dab7259
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-19 16:24:57 +1200

    Added javadoc for the remaining 5 crop types.

--- farm/src/main/crops/Wasabi.java
+++ farm/src/main/crops/Wasabi.java
@@ -1,3 +1,11 @@
+/**
+ * This is an extension of the Crop class.
+ * It is an item that can be bought in game.
+ * Once its daystoHarvest reaches 0, it can be harvested for money.  
+ * 
+ * @author Grant Wong
+ */
+
 package main.crops;
 
 public class Wasabi extends Crop {
