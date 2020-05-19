95c8c914d32763993bcb410c1c62d171e16f5bc3
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-19 20:40:47 +1200

    Item JavaDoc readied.

--- farm/src/main/items/Item.java
+++ farm/src/main/items/Item.java
@@ -1,3 +1,7 @@
+/**
+ * a child class of asset which can be bought from stores and consumed for player benefit
+ * Divided into animal and crop items, they benefit these groups differently
+ */
 package main.items;
 
 import main.Asset;
@@ -7,7 +11,14 @@ public class Item extends Asset {
 	private String type;
 	private String benefit;
 	private String flavour;
-
+	/**
+	 * 
+	 * @param name - of the item
+	 * @param purchasePrice - price to buy
+	 * @param type - used on animals or crops
+	 * @param benefit - the function of the item
+	 * @param flavour - a description of the item
+	 */
 	public Item(String name, int purchasePrice, String type,
 			String benefit, String flavour) {
 		super(name, purchasePrice);
@@ -15,24 +26,42 @@ public class Item extends Asset {
 		this.benefit = benefit;
 		this.flavour = flavour;
 	}
-
+	
+	/**
+	 * 
+	 * @return type of item in string
+	 */
 	public String getType() {
 		return type;
 	}
-
+	
+	/**
+	 * 
+	 * @return benefit description in a string
+	 */
 	public String getBenefit() {
 		return benefit;
 	}
-
+	
+	/**
+	 * 
+	 * @return return flavour text of item
+	 */
 	public String getFlavour() {
 		return flavour;
 	}
 
+	/**
+	 * @return return a string representation of the item
+	 */
 	@Override
 	public String toString() {
 		return name + ": " + benefit;
 	}
-
+	
+	/**
+	 * @return return a detailed string representation of the item
+	 */
 	@Override
 	public String getInfo() {
 		return name + ": (for " + type + ")\n" + benefit + "\n" + flavour;
