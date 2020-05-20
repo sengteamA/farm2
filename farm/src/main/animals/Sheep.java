02639c7349d8f83d615cd9983d2ed137362cf31b
Grant Wong <git@wong.nz>
2020-05-20 20:05:01 +1200

    Add full documentation for animals

--- farm/src/main/animals/Sheep.java
+++ farm/src/main/animals/Sheep.java
@@ -1,5 +1,10 @@
 package main.animals;
 
+/**
+ * A sheep. The cheapest of all the animals, and its stats are the lowest too.
+ *
+ * @author Nick
+ */
 public class Sheep extends Animal {
 	public Sheep() {
 		super("Sheep", 50, 50, 100, 100, 200);
