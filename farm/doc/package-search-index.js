240c2a981250e1a72839295cf8394cb766fd2a8c
Grant Wong <git@wong.nz>
2020-05-20 21:43:39 +1200

    Update javadoc

--- farm/doc/package-search-index.js
+++ farm/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"main.animals"},{"l":"main.items"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"main.items"}]
