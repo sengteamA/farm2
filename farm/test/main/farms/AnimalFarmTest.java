1fad0cc5cd9e6fb6228eb10a85ea10f0ad86493b
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-21 12:15:15 +1200

    updated comments for AnimalFarmTest

--- farm/test/main/farms/AnimalFarmTest.java
+++ farm/test/main/farms/AnimalFarmTest.java
@@ -9,26 +9,29 @@ import org.junit.jupiter.api.Test;
 class AnimalFarmTest {
 
 	public AnimalFarm snowball;
-
+	//initialised object before each test
 	@BeforeEach
 	public void init() {
 		snowball = new AnimalFarm();
 	}
 
-	@Test // passed 16/04/2020
+	//test that flavour string is correct
+	@Test 
 	void flavourTest() {
 		String s = snowball.getFlavour();
 		assertEquals("All Animals are equal, but some are more equal than others.\n"
 				+ "All animals start 20% happier and healthier on purchase.\n"
 				+ "Feeding and playing with animals is 20% more effective.\n", s);
 	}
-
-	@Test // passed 02/05/2020
+	
+	//test inheritance correctly implemented
+	@Test
 	void class_test() {
 		assertTrue(snowball instanceof AnimalFarm);
 		assertTrue(snowball instanceof Farm);
 	}
 	
+	//test that animalBonus correctly implemented
 	@Test 
 	void animalBonusTest() {
 		assertEquals((float)0.2, snowball.getAnimalBonus());
