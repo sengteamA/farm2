6dc4afb3aabf8e99294b1b8dcdb027e2fdb872cd
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-21 22:03:43 +1200

    Corrected class name

--- farm/test/main/AssetTests.java
+++ farm/test/main/AssetTests.java
@@ -3,7 +3,7 @@ package main;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class AssetTests {
+class AssetTest {
 	
 	// Tests custom asset is displayed correctly.
 	@Test
