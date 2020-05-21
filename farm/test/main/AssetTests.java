2c4312f59002ce1243043c9b967c489713798d1b
AbysmalMonkey <nicklee9999@hotmail.com>
2020-05-21 21:59:08 +1200

    Added AssetTests.
    Asset cover 100%.

---
+++ farm/test/main/AssetTests.java
@@ -0,0 +1,22 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class AssetTests {
+	
+	// Tests custom asset is displayed correctly.
+	@Test
+	void getInfoTest() {
+		Asset car = new Asset("Car", 10000);
+		assertEquals("Car - 10000", car.getInfo());
+	}
+	
+	// Tests the to String method
+	@Test
+	void toStringTest() {
+		Asset car = new Asset("Car", 10000);
+		assertEquals("Car: 10000", car.toString());
+	}
+
+}
